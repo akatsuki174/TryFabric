94a3d3ad3520024bc1828fc6f4abd0abd9e0ac28
akatsuki174 <dev2015js@gmail.com>
2017-07-18 15:07:02 +0900

    STEP2の実装を追加

--- TryFabric/ViewController.swift
+++ TryFabric/ViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import Optimizely
+
+// Creates a Live Variable with a given key and default value
+// that will be accessable throughout your target
+internal var messageKey = OptimizelyVariableKey.optimizelyKey(withKey: "message", defaultNSString: "Hello World!")
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let messageButton = UIButton(type: .system)
+        messageButton.setTitle("Show Message", for: [])
+        messageButton.addTarget(self, action: #selector(self.showMessage(_:)), for: .touchUpInside)
+        messageButton.frame = CGRect(x: 25, y: 60, width: 200, height: 50)
+        view.addSubview(messageButton)
 
     }
 
@@ -20,5 +31,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showMessage(_ sender: Any) {
+        let alert = UIAlertController(title: "Live Variable",
+                                      message: Optimizely.string(for: messageKey),
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: .default,
+                                      handler: nil))
+        present(alert, animated: true) {}
+    }
+    
 }
 
